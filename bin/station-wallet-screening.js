35b88669e9e83838325d48f15e6ba4519cf12938
Julian Gruber <julian@juliangruber.com>
2023-11-05 15:57:50 +0100

    add chainalysis

--- bin/station-wallet-screening.js
+++ bin/station-wallet-screening.js
@@ -1,10 +1,17 @@
 import http from 'node:http'
 import { once } from 'node:events'
-import { handler } from '../index.js'
+import { createHandler } from '../index.js'
+import assert from 'node:assert'
 
-const { PORT = 3000 } = process.env
+const {
+  PORT = 3000,
+  CHAINALYSIS_API_KEY
+} = process.env
+assert(CHAINALYSIS_API_KEY)
 
-const server = http.createServer(handler)
+const server = http.createServer(createHandler({
+  apiKey: CHAINALYSIS_API_KEY
+}))
 server.listen(PORT)
 await once(server, 'listening')
 console.log(`http://127.0.0.1:${PORT}`)
---
+++ test/unit.js
@@ -0,0 +1,98 @@
+import { createHandler } from '../index.js'
+import http from 'node:http'
+import { once } from 'node:events'
+import assert from 'node:assert'
+
+describe('Unit tests', () => {
+  let server
+  let port
+
+  before(async () => {
+    server = http.createServer()
+    server.listen()
+    await once(server, 'listening')
+    ;({ port } = server.address())
+  })
+  after(() => server.close())
+
+  it('allows wallets', async () => {
+    const fetchCalls = []
+    server.once('request', createHandler({
+      async fetch (url, opts) {
+        fetchCalls.push({ url, opts })
+        return {
+          ok: true,
+          status: 200,
+          json: async () => ({ identifications: [] })
+        }
+      }
+    }))
+
+    const { status } = await fetch(`http://127.0.0.1:${port}/0xADDRESS`)
+    assert.strictEqual(status, 200)
+
+    assert.deepStrictEqual(fetchCalls, [{
+      url: 'https://public.chainalysis.com/api/v1/address/0xADDRESS',
+      opts: {
+        headers: {
+          'X-API-Key': undefined,
+          accept: 'application/json'
+        }
+      }
+    }])
+  })
+
+  it('disallows wallets', async () => {
+    const fetchCalls = []
+    server.once('request', createHandler({
+      async fetch (url, opts) {
+        fetchCalls.push({ url, opts })
+        return {
+          ok: true,
+          status: 200,
+          json: async () => ({ identifications: [{}] })
+        }
+      }
+    }))
+
+    const { status } = await fetch(`http://127.0.0.1:${port}/0xADDRESS`)
+    assert.strictEqual(status, 403)
+
+    assert.deepStrictEqual(fetchCalls, [{
+      url: 'https://public.chainalysis.com/api/v1/address/0xADDRESS',
+      opts: {
+        headers: {
+          'X-API-Key': undefined,
+          accept: 'application/json'
+        }
+      }
+    }])
+  })
+
+  it('handles API failures', async () => {
+    const fetchCalls = []
+    server.once('request', createHandler({
+      async fetch (url, opts) {
+        fetchCalls.push({ url, opts })
+        return {
+          ok: false,
+          status: 500
+        }
+      },
+      log () {}
+    }))
+
+    const { status } = await fetch(`http://127.0.0.1:${port}/0xADDRESS`)
+    assert.strictEqual(status, 500)
+
+    assert.deepStrictEqual(fetchCalls, [{
+      url: 'https://public.chainalysis.com/api/v1/address/0xADDRESS',
+      opts: {
+        headers: {
+          'X-API-Key': undefined,
+          accept: 'application/json'
+        }
+      }
+    }])
+  })
+})
